477ba8e1c7e3ef193c657a735000c0da2677df29
Emilio Henrique Correa <emiliohc@pbtech.net.br>
2023-01-31 13:58:36 -0300

    refactor: 💡 change test name

--- src/components/Dots/Dots.test.tsx
+++ src/components/Dots/Dots.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@/test/utils';
 import { Dots } from './Dots';
 
-test.each([undefined, 1, 2, 3])('renderiza dots com %s', (value) => {
+test.each([undefined, 1, 2, 3])('render dots with %s', (value) => {
   const container = render(<Dots dots={value} />);
 
   expect(container).toMatchSnapshot();
