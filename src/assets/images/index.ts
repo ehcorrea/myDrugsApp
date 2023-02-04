3fcf87070ec43899e23b6b406d5d39f8946ecf43
Emilio Henrique Correa <emiliohc@pbtech.net.br>
2023-02-04 19:48:12 -0300

    feat: add svgs

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,1 +1,4 @@
+export { default as IconBack } from './iconBack.svg';
+export { default as IconPasswordEye } from './iconPasswordEye.svg';
+export { default as IconPasswordEyeHide } from './iconPasswordEyeHide.svg';
 export { default as InitialBackground } from './initialBackground.jpeg';
