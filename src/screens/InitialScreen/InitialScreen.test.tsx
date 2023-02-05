5f3fd69c6d76a2d293e4cebadde5bd90c6b17921
Emilio Henrique Correa <emiliohc@pbtech.net.br>
2023-02-05 17:17:11 -0300

    test: update test in InitialScreen with navigation

--- src/screens/InitialScreen/InitialScreen.test.tsx
+++ src/screens/InitialScreen/InitialScreen.test.tsx
@@ -1,9 +1,20 @@
-import { render } from '@/test/utils';
+import { fireEvent, mockedNavigation, render } from '@/test/utils';
 
 import { InitialScreen } from './InitialScreen';
 
-test('should render screen with success', () => {
+const setup = () => {
   const container = render(<InitialScreen />);
+  const button = container.getByLabelText('Get Started');
+
+  return { button, ...container };
+};
+
+test('should render screen with success', () => {
+  const container = setup();
   expect(container.getByLabelText('inicial screen banner')).toBeTruthy();
-  expect(container.getByLabelText('Get Started')).toBeTruthy();
+
+  fireEvent.press(container.button);
+  expect(mockedNavigation.navigate).toHaveBeenCalledWith('LoginScreen');
+
+  expect(container).toMatchSnapshot();
 });
