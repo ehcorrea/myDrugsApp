a92176086cae1ea558769446deaf8b872e5289fd
Emilio Henrique Correa <emiliohc@pbtech.net.br>
2023-02-05 19:51:23 -0300

    feat: add icons for login

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,7 @@
 export { default as IconBack } from './iconBack.svg';
+export { default as IconLoginApple } from './iconLoginApple.svg';
+export { default as IconLoginFacebook } from './iconLoginFacebook.svg';
+export { default as IconLoginGoogle } from './iconLoginGoogle.svg';
 export { default as IconPasswordEye } from './iconPasswordEye.svg';
 export { default as IconPasswordEyeHide } from './iconPasswordEyeHide.svg';
 export { default as InitialBackground } from './initialBackground.jpeg';
