afec3c9bf3ade4cac02041805e519928433d6664
Emilio Henrique Correa <ehcorrea60@gmail.com>
2023-02-26 23:07:19 -0300

    feat: export SpacingsZize

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -4,3 +4,4 @@ export type BorderRadius = keyof typeof theme.border.radius;
 export type Color = keyof typeof theme.colors;
 export type FontFamily = keyof typeof theme.font.family;
 export type FontSize = keyof typeof theme.font.sizes;
+export type SpacingsZize = keyof typeof theme.spacings;
